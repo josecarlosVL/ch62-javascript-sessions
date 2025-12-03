6712cbde2b42c49d70287432156f79ee84cd4490
Jose Carlos Velasco <jcvl0611@gmail.com>
2025-12-03 12:39:12 -0600

    Feat: Introduction to Objects

--- js-05-oop/src/js/main.js
+++ js-05-oop/src/js/main.js
@@ -4,5 +4,7 @@
 //Importación nombrada
 import { changePrincipalTitle } from "./modules/changePrincipalTitle.js"
 import { changeLogo } from "./modules/changeLogo.js";
+import { aliceUser } from "./modules/literalObject.js";
 changePrincipalTitle("title");
 changeLogo("bob.jpg");
+console.log(aliceUser.getFullName());
